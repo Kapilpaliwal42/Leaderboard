d939c4a884523660833c94b842eb789895b98f1a
Kapil Paliwal <162558180+Kapilpaliwal42@users.noreply.github.com>
2025-08-19 21:58:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,20 @@ import DBConnect from "./db.js/DBConnect.js"
 dotenv.config();
 const port = process.env.PORT || 5000;
 
+const allowedOrigins = ['https://leaderboard-frontend-ggp8.onrender.com'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+};
+
 const app = express();
 try {
     await DBConnect();
